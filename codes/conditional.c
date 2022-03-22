3759b6f607e45187f2d13e6dbfd6d13a78d9b168
Francis Tin-ao <70809588+francistinao@users.noreply.github.com>
2022-03-22 23:19:29 +0800

    conditional statements practice

---
+++ codes/conditional.c
@@ -0,0 +1,109 @@
+// /* Conditional Statements Programming Practice */
+
+// /*  Write a C program to accept two integers 
+// and check whether they are equal or not. */
+
+ #include <stdio.h>
+
+ int main () {
+     int num1,num2;
+
+     printf("Enter a num: ");
+     scanf("%d",&num1);
+
+     printf("Enter second num: ");
+     scanf("%d",&num2);
+
+     //If else statement
+     if (num1 == num2) {
+         printf("Number1 and Number2 are equal");
+     } else {
+         printf("Num1 and Num2 are not equal");
+     }
+ }
+
+// /* Write a C program to check wheter a given
+// number is even or odd */
+
+ #include <stdio.h>
+
+ int main() {
+     int num1;
+
+     printf("Enter a number: ");
+     scanf("%d",&num1);
+
+
+     if(num1%2==0){
+         printf("%d is an even integer");
+     } else {
+         printf("%d is an odd integer");
+     }
+
+     return 0;
+ }
+
+// /* Write a C program to check whether a given
+// number is positive or negative */
+
+// #include <stdio.h>
+
+ int main() {
+     int num;
+
+     printf("Enter a number: ");
+     scanf("%d",&num);
+    
+     //Conditional statement
+     if(num<=0){
+         printf("%d is a negative integer",num);
+     } else {
+         printf("%d is a positive integer",num);
+     } 
+
+//     return 0;
+// }
+
+/*
+Write a C program to find the largest of the
+three numbers*/
+
+//Since the program needed 3 input numbers 
+//we will declare 3 variables with its
+//acceptable identifiers
+
+#include <stdio.h>
+
+int main() {
+    int num1,num2,num3; //so as you can see here
+    // I declare 3 variables
+
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+    
+    printf("Enter third number: ");
+    scanf("%d",&num3);
+
+    //since we all have these three numbers
+    //now we have to build the logic
+    //to identify which of the three numbers
+    //is the largest
+
+    printf("1st Number = %d",num1);
+    printf("\n2nd Number = %d",num2);
+    printf("\n3rd Number = %d",num3);
+
+    if (num1 > num2 && num1 > num3){
+        printf("\nThe 1st Number is the greatest among the three");
+    } else if (num2 > num1 && num2 > num3){
+        printf("\nThe 2nd number is the greatest among the three");
+    } else if (num3 > num1 && num3 > num2){
+        printf("\nThe 3rd number is the greatest among the three");
+    } else {
+        return 0;
+    }
+
+}
