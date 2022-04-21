20dab4c307b4df2c54d6f0f496de5d6d8915e253
Francis Tin-ao <70809588+francistinao@users.noreply.github.com>
2022-04-22 00:29:17 +0800

    much better logic!

--- codes/number_guessing_game.c
+++ codes/number_guessing_game.c
@@ -3,26 +3,36 @@ logic by Francis Tin-ao */
 #include <stdio.h>
 
 int main(){
-    int lucky_number = 5,guesses = 3,guess;
-
+    int lucky_number = 70,guesses = 2,guess;
+    
+    printf("Try to guess the lucky number!");
     while(guess!=lucky_number) //Since we haven't guessed yet the lucky nubmer 
         {
         printf("\nEnter your guess: ");
         scanf("%d",&guess);
         printf("Guess left: %d\n",guesses);
         
-        if(guesses!=0){ //Since var guess is still 3 and not equal to 0
+        if(guesses!=0){ //Since guess is still 3 and not equal to 0
         //the user can prompt
-            if (guess!=lucky_number){  //Nested if else 
-                printf("Try again");
-                guesses--;  //decrements the value of guesses to satisfy the parent else condition
+            if (guess != lucky_number){
+                if(guess < lucky_number){
+                    printf("\nYour guess is less than the lucky number"); //Prompts the user that the inputted guess is less than the lucky number
+                } else {
+                    printf("\nYour guess is greater than the lucky number"); //Prompts the user that the inputted guess is greater than the lucky number
+                }
+                printf("\nTry again");
+                guesses--; //Decrements the guesses
+               
             } else {
                 printf("Congratulations. You won!");
             } 
-        } else { //Is guess equal to 0? 
+        } else if (guess == lucky_number && guesses == 0){ 
+            printf("What a clutch! Congratulations"); 
+            break;
+        } else {
             printf("You have no more guesses left. You lose :(");
             break; //Terminates the loop if the value of guessues satisfy the condition
         }
-    }
+    } 
     return 0;
 }
