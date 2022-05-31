248890ba286adce83e86f70a93afb1df851a4d3a
Francis Tin-ao <franfra10j@gmail.com>
2022-05-31 10:47:51 +0800

    update

--- banking_system.c
+++ banking_system.c
@@ -1,8 +1,11 @@
-/* This is a C Program that 
+/* This is a C Program that
 contains the functionalities of how
 a banking system works. It consist
-of all basics of fundamentals of programming 
+of all the basics of fundamentals of programming
 
+The program couldn't create multiple client
+accounts since the program only consist 1 sub variable
+for the structure
 
 Program made by Francis Tin-ao
 */
@@ -11,28 +14,60 @@ Program made by Francis Tin-ao
 #include <string.h>
 #include <math.h>
 
- struct Client {
+struct Client {
      char name[10];
      int bankPin[4];
      double balance,loan;
 }client;
 
-// int client_loan(struct Client) {
-//     const float interest = 1.6;
-//     float amount_to_pay,loan;
-//     int months = 12;
+int client_pay_loan(struct Client) {
+    if(client.balance != 0) {
+        if(client.balance > client.loan) {
+            client.balance-=client.loan;
+            printf("You have successfully paid your loan.\n");
+            printf("PHP %.2f is deducted on your balance",client.loan);
+            client.loan = 0;
+        } else {
+            printf("Your balance is not enough to pay the loan.\n");
+        }
+        bank_login_successful(client);
+    } else {
+        printf("You have no balance to pay the loan");
+        bank_login_successful(client);
+    }
+}
 
-//     printf("Enter amount to loan: ");
-//     scanf("%f",&loan);
+int client_loan(struct Client) {
+    const float interest = 0.05; //5% interest rate
+    float amount_to_pay,loan,sum = 0,init_loan_val; //initial loan value where the interest loan value is not yet added
+    int months = 12, i = 1;
 
-//     printf("You have loan: PHP%.2f\n\n",loan);
+    printf("Enter amount to loan: ");
+    scanf("%f",&loan);
+
+    printf("Interest: %5\n");
+    printf("You have loaned: PHP%.2f\n\n",loan);
+    init_loan_val = loan; //init_loan_val is now holdign the value of loan where the user inputted the amount borrowed
     
-//     for(int i = 1; i <= months; i++){
-//         loan*=interest;
-//     }
-//     amount_to_pay = loan;
-//     printf("You must pay PHP%.2f by the end of the year",amount_to_pay);
-// }
+    loan*=interest;
+
+    /*let us say for example, i have loaned 50000 Fifty thousand since we have 5% interest rate
+    Now we have 25000 interest rate then we will multiply it by 12 since the interest rate must be
+    applied monthly thats good for 1 year, then in the standard interest rate, the updated
+    amount will be added to the original loan amount which is 50000 + 30000 = 80000, therefor the 
+    client must pay 80000 by the end of the year */
+
+    while(i <= months) {
+        loan*=1; //For us to be able to grab only the value of the loan, we just have to multiply it by 1
+        sum+=loan; //THis will compute the sum of the loan times the months 
+        i++;
+    }
+    client.loan = init_loan_val + sum; //In this step, we will now add the original value borrowed for the user and the updated amount where interest is applied
+    printf("You must pay PHP %.2f by the end of the year",client.loan);
+
+    bank_login_successful(client);
+
+}
 
 int client_withdraw(struct Client) {
     const float MIN_WITH_AMOUNT = 1000;
@@ -43,23 +78,29 @@ int client_withdraw(struct Client) {
     printf("Enter amount: ");
     scanf("%f",&amount);
 
-    if(amount >= MIN_WITH_AMOUNT) {
-        if(amount <= client.balance) {
-            client.balance-=amount;
-            printf("The withdraw is successful");
-            bank_login_successful(client);
-        } else {
-            printf("The amount inputted is GREATER than the client's balance");
+    if(client.balance != 0) {
+        if(amount >= MIN_WITH_AMOUNT) {
+            if(amount <= client.balance) {
+                client.balance-=amount;
+                printf("The withdraw is successful");
+                bank_login_successful(client);
+            } else {
+                printf("The amount inputted is GREATER than the client's balance");
+                bank_login_successful(client);
+                }
+        } else if (amount < MIN_WITH_AMOUNT) {
+            printf("The amount inputted did not exceed the MINIMUM Withdrawal amount");
             bank_login_successful(client);
         }
-    } else if (amount < MIN_WITH_AMOUNT) {
-        printf("The amount inputted did not exceed the MINIMUM Withdrawal amount");
-        bank_login_successful(client);
+    } else {
+        printf("The client's balance is empty");
+        bank_options(client);
     }
+
 }
 
 int client_deposit(struct Client) {
-    const float MIN_AMOUNT = 500; 
+    const float MIN_AMOUNT = 500;
     float amount;
     printf("---DEPOSIT---\n");
     printf("The amount you deposite must be 500 in MINIMUM\n\n");
@@ -106,13 +147,17 @@ int bank_options(struct Client) {
             break;
 
         case 4:
-            // client_loan(client);
+            client_loan(client);
             break;
 
         case 5:
+            client_pay_loan(client);
+            break;
+
+        case 6:
             main();
             break;
-    
+
         default:
             printf("Invalid choice");
             bank_options(client);
@@ -121,38 +166,38 @@ int bank_options(struct Client) {
 }
 
 int bank_login_successful(int choice, struct Client) {
+    printf("\n\nWELCOME!");
     printf("\nName: ");
     printf("%s \n",client.name);
 
     printf("Balance: ");
-    printf("PHP%.2lf\n",client.balance);
+    printf("PHP %.2lf\n",client.balance);
 
-    printf("Amount that must be paid: ");
-    printf("%.2lf",client.loan);
+    printf("Client loan: ");
+    printf("PHP %.2lf",client.loan);
 
     bank_options(client);
 }
 
 
  int bank_account_signup(int choice, struct Client) {
-     printf("Name: ");
+     printf("Enter First Name: ");
      scanf("%s",&client.name);
 
-     printf("Create PIN:  ");
+     printf("Create 4-Num PIN:  ");
      scanf("%d",client.bankPin);
      printf("Account successfully created!\n");
-     
+
      main();
 }
 
 int bank_account_login(int choice,struct Client) {
-    char name[10];
     int inputPin[4],client_choice;
     int tries = 3;
 
     printf("Enter PIN: ");
     scanf("%d",&inputPin);
-    
+
     for(int i = 0; i < sizeof(inputPin); i++){
         if(inputPin[i] == client.bankPin[i]) {
             if(1) {
@@ -160,7 +205,7 @@ int bank_account_login(int choice,struct Client) {
                break;
             }
         } else {
-            printf("Invalid user\n\n");
+            printf("Invalid! No existing client\n\n");
             printf("Would you like to login again? \n[1]Yes\n[2]No\n\nChoice: ");
             scanf("%d",&client_choice);
 
@@ -175,7 +220,7 @@ int bank_account_login(int choice,struct Client) {
                     if(inputPin[i]==client.bankPin[i]) {
                        bank_login_successful(choice,client);
                        break;
-                    } 
+                    }
                     printf("Invalid user");
                 }
                 printf("\nYou have exceeded the limit of re-entering PIN\n\n\n");
@@ -188,11 +233,10 @@ int bank_account_login(int choice,struct Client) {
     }
 }
 
-
 int main(void) {
-  int choice; 
- 
-  printf("\nWelcome to Furashizu National Bank\n\nHow may I help you? \n[1] Login\n[2] Create an account");
+  int choice;
+
+  printf("\nWelcome to Garuda National Bank\n\nHow may I help you? \n[1] Login\n[2] Create an account");
   printf("\n\nChoice : ");
   scanf("%d",&choice);
 
@@ -205,7 +249,12 @@ int main(void) {
       case 2:
         bank_account_signup(choice,client);
         break;
+
+      default: 
+        printf("Invalid choice\n\n");
+        main();
+        break;
   }
-    
+
   return 0;
 }
