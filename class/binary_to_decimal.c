5c60a8e1ea7438b12522a38ac5728e0b055e2176
Francis Tin-ao <franfra10j@gmail.com>
2022-05-31 16:00:53 +0800

    binary to decimal convertion

--- class/binary_to_decimal.c
+++ class/binary_to_decimal.c
@@ -5,20 +5,21 @@ int bin_to_dec(char *binary) {
     const int bin_base = 2; 
     int i = strlen(binary) - 1, 
     total_dec_val = 0, 
-    index_value = 1;
-    
+    binary_value = 1;
+
     while(i >= 0) {
         switch(binary[i]) {
             case '1':
-               total_dec_val+=index_value;
+               total_dec_val+=binary_value;
                break;
 
             case '0': 
                break;
         }
-        index_value *= bin_base;
+        binary_value *= bin_base;
         i-=1;
     }
+    
     return total_dec_val;
 }
 
