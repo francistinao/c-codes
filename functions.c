444765b2a2d1e0a9b853421fc6d82f38e04f367e
Francis Tin-ao <franfra10j@gmail.com>
2022-05-31 10:47:39 +0800

    update

---
+++ functions.c
@@ -0,0 +1,34 @@
+// #include <stdio.h>
+
+// int pyramid_of_asterisk(int x, int y) {
+//     for(int i = 0; i < x; i++) {
+//         for(int j = y; j > i; j--) {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int value_of_x_and_y(int x, int y) {
+//     printf("Enter number for X Axis: ");
+//     scanf("%d",&x);
+
+//     printf("Enter number for Y Axis: ");
+//     scanf("%d",&y);
+
+//     pyramid_of_asterisk(x,y);
+// }
+
+// int main() {
+//     int x,y;
+//     value_of_x_and_y(x,y);
+// }
+
+#include <stdio.h>
+
+int main() {
+    int bin, dec;
+
+    printf("Enter a binary number: ");
+    scanf("%d", &bin);
+}
