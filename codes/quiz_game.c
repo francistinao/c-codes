99579397c310b3777a92a4c733bf5e965ca53f7f
Francis Tin-ao <franfra10j@gmail.com>
2022-06-10 23:16:02 +0800

    practicing functions using recursion

---
+++ codes/quiz_game.c
@@ -0,0 +1,23 @@
+/* Write a C program and create a quiz game, make your lives into 5
+and create questions about computer system with an answer , 
+write your program in the box below */
+
+#include <stdio.h>
+
+ struct Ques {
+    char ans[5][20]; 
+}answer1,answer2,answer3,answer4;
+
+char questions(void);
+
+int main() {
+    int lives = 5;
+
+    while(lives != 0) {
+        
+    }
+}
+
+char questions() {
+    Ques.answer1 = "Francis";
+}
